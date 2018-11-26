ff7c975603c90d7ec875aca21ca3071b04b8fa0c
pcshao <shaocover@163.com>
2018-11-26 14:53:37 +0800

    add admin.properties

--- gra_business/src/main/java/cn/pcshao/graduaction/BusinessApplication.java
+++ gra_business/src/main/java/cn/pcshao/graduaction/BusinessApplication.java
@@ -4,11 +4,22 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @SpringBootApplication
 @MapperScan("cn.pcshao.grant.common.dao")
 public class BusinessApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BusinessApplication.class, args);
+    public static void main(String[] args) throws IOException {
+        //更改properties配置文件名称,避免依赖冲突
+        Properties properties = new Properties();
+        InputStream in = BusinessApplication.class.getClassLoader().getResourceAsStream("admin.properties");
+        properties.load(in);
+        SpringApplication app = new SpringApplication(BusinessApplication.class);
+        app.setDefaultProperties(properties);
+        app.run(args);
+//        SpringApplication.run(CommonApplication.class, args);
     }
 }

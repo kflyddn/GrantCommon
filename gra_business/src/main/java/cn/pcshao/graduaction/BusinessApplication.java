aba2cbfd1e3befe1c4328a934a400cf451ea6834
pcshao <shaocover@163.com>
2018-11-26 19:05:13 +0800

    update some pom.xml

--- gra_business/src/main/java/cn/pcshao/graduaction/BusinessApplication.java
+++ gra_business/src/main/java/cn/pcshao/graduaction/BusinessApplication.java
@@ -1,8 +1,12 @@
 package cn.pcshao.graduaction;
 
+import org.apache.tomcat.util.descriptor.web.ErrorPage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,7 +19,7 @@ public class BusinessApplication {
     public static void main(String[] args) throws IOException {
         //更改properties配置文件名称,避免依赖冲突
         Properties properties = new Properties();
-        InputStream in = BusinessApplication.class.getClassLoader().getResourceAsStream("admin.properties");
+        InputStream in = BusinessApplication.class.getClassLoader().getResourceAsStream("business.properties");
         properties.load(in);
         SpringApplication app = new SpringApplication(BusinessApplication.class);
         app.setDefaultProperties(properties);

fd9afbf725be20b63758f26a8d499c60006cdbb7
pcshao <shaocover@163.com>
2018-12-14 16:39:05 +0800

    add static source.

--- gra_business/src/main/resources/static/static/js/systemManage.js
+++ gra_business/src/main/resources/static/static/js/systemManage.js
@@ -2,21 +2,22 @@
     layui.use('table', function () {
         table = layui.table;
 
+        //用户列表渲染
         table.render({
-            elem: '#tableData',
+            elem: '#tableUser',
             url: '/user/queryUser',
             method: 'post',
             contentType: 'application/json',
             page: true,
             cols: [[
-                {field: 'userId', title: 'ID',  fixed: 'left', sort: true},
+                {field: 'userId', title: 'ID', fixed: 'left', sort: true},
                 {field: 'username', title: '用户账号'},
                 {field: 'nickname', title: '用户姓名'},
                 {field: 'sex', title: '性别'},
                 {field: 'email', title: '邮箱地址'},
                 {field: 'tel', title: '电话号码'},
                 {field: 'isUse', title: '可用'},
-                {fixed: 'right', width: 215, align: 'center', toolbar: '#userOption'},
+                    {fixed: 'right', width: 150, align: 'center', toolbar: '#userOption'},
             ]],
             parseData: function (res) {
                 return {
@@ -34,7 +35,64 @@
                 statusCode: 10
             }
         });
-
+        //角色列表渲染
+        table.render({
+            elem: '#tableRole',
+            url: '/user/queryRole',
+            method: 'post',
+            contentType: 'application/json',
+            page: true,
+            cols: [[
+                {field: 'roleId', title: 'ID', fixed: 'left', sort: true},
+                {field: 'roleName', title: '角色名'},
+                {field: 'roleRemark', title: '角色描述'},
+                {fixed: 'right', width: 150, align: 'center', toolbar: '#userOption'},
+            ]],
+            parseData: function (res) {
+                return {
+                    "code": res.code,
+                    "msg": res.msg,
+                    "data": res.data,
+                    "count": res.data.size,
+                };
+            },
+            request:{
+                pageName: 'pageNum', //页码的参数名称，默认：page
+                limitName: 'pageSize' //每页数据量的参数名，默认：limit
+            },
+            response:{
+                statusCode: 10
+            }
+        });
+        //权限列表渲染
+        table.render({
+            elem: '#tablePermission',
+            url: '/user/queryPermission',
+            method: 'post',
+            contentType: 'application/json',
+            page: true,
+            cols: [[
+                {field: 'permissionId', title: 'ID', fixed: 'left', sort: true},
+                {field: 'permissionName', title: '权限名'},
+                {fixed: 'right', width: 150, align: 'center', toolbar: '#userOption'},
+            ]],
+            parseData: function (res) {
+                return {
+                    "code": res.code,
+                    "msg": res.msg,
+                    "data": res.data,
+                    "count": res.data.size,
+                };
+            },
+            request:{
+                pageName: 'pageNum', //页码的参数名称，默认：page
+                limitName: 'pageSize' //每页数据量的参数名，默认：limit
+            },
+            response:{
+                statusCode: 10
+            }
+        });
+        //系统日志列表渲染
         table.render({
             elem: '#tableDataLog',
             // url: '/allSysLogByPage', //数据接口
@@ -59,22 +117,27 @@
             },
         });
 
+        //监听toolbar 筛选
         table.on('tool(user)', function(obj){
             var data = obj.data;
             if(obj.event === 'del'){
-                layer.confirm("确认删除用户"+obj.data.loginname, {btn: ['确定', '取消'],title:"提示"},function () {
+                layer.confirm("确认删除用户"+obj.data.username, {btn: ['确定', '取消'],title:"提示"},function () {
+                    if("admin" == data.username)
+                        return;
+                    var userIdList = data.userId;
                     $.ajax({
-                        url: '/user/delete',
+                        url: '/user/removeUser',
                         type: 'post',
-                        data: {id:data.id},
+                        data: {
+                            userIdList: userIdList
+                        },
                         dataType: 'json',
                         success: function (result) {
-                            if(result.code == 0){
+                            if(result.code == 10){
                                 window.location.reload()
                             }else {
                                 layer.alert(result.msg);
                             }
-
                         },
                         failure : function() {
                             layer.alert('操作超时!');
@@ -82,17 +145,88 @@
                     });
                 });
             }else if(obj.event === 'edit'){
-                if('admin'!=data.loginname) {
-                    $("#userEdit input[name ='loginname']").val(data.loginname);
-                    $("#userEdit input[name ='pwd']").val(data.pwd);
-                    $("#userEdit input[name ='rePwd']").val(data.pwd);
-                    $("#userEdit input[name ='realname']").val(data.realname);
+                if('admin'!=data.username) {
+                    $("#userEdit input[name ='loginname']").val(data.username);
+                    $("#userEdit input[name ='pwd']").val(data.password);
+                    $("#userEdit input[name ='rePwd']").val(data.password);
+                    $("#userEdit input[name ='realname']").val(data.nickname);
+                    $("#userEdit input[name ='realname']").val(data.sex);
+                    $("#userEdit input[name ='dsc']").val(data.email);
                     $("#userEdit input[name ='tel']").val(data.tel);
-                    $("#userEdit input[name ='dsc']").val(data.dsc);
-                    $("#userEdit input[name ='id']").val(data.id);
+                    $("#userEdit input[name ='dsc']").val(data.isUse);
+                    $("#userEdit input[name ='id']").val(data.userId);
                     $("#userEdit").modal();
                 }else{
                     layer.alert('系统用户默认不能编辑！')
+                }
+            }
+        });
+        table.on('tool(role)', function(obj){
+            var data = obj.data;
+            if(obj.event === 'del'){
+                layer.confirm("确认删除角色"+obj.data.roleName, {btn: ['确定', '取消'],title:"提示"},function () {
+                    var roleIdList = data.roleId;
+                    $.ajax({
+                        url: '/user/removeRole',
+                        type: 'post',
+                        data: {
+                            roleIdList: roleIdList
+                        },
+                        dataType: 'json',
+                        success: function (result) {
+                            if(result.code == 10){
+                                window.location.reload()
+                            }else {
+                                layer.alert(result.msg);
+                            }
+                        },
+                        failure : function() {
+                            layer.alert('操作超时!');
+                        }
+                    });
+                });
+            }else if(obj.event === 'edit'){
+                if('admin'!=data.roleName) {
+                    $("#userEdit input[name ='loginname']").val(data.roleName);
+                    $("#userEdit input[name ='pwd']").val(data.roleRemark);
+                    $("#userEdit input[name ='id']").val(data.roleId);
+                    $("#userEdit").modal();
+                }else{
+                    layer.alert('系统默认最高权限角色不能编辑！')
+                }
+            }
+        });
+        table.on('tool(permission)', function(obj){
+            var data = obj.data;
+            if(obj.event === 'del'){
+                layer.confirm("确认移除权限"+obj.data.permissionName, {btn: ['确定', '取消'],title:"提示"},function () {
+                    var permissionIdList = data.userId;
+                    $.ajax({
+                        url: '/user/removeUser',
+                        type: 'post',
+                        data: {
+                            permissionIdList: permissionIdList
+                        },
+                        dataType: 'json',
+                        success: function (result) {
+                            if(result.code == 10){
+                                window.location.reload()
+                            }else {
+                                layer.alert(result.msg);
+                            }
+                        },
+                        failure : function() {
+                            layer.alert('操作超时!');
+                        }
+                    });
+                });
+            }else if(obj.event === 'edit'){
+                if('系统管理'!=data.permissionName) {
+                    $("#userEdit input[name ='loginname']").val(data.permissionName);
+                    $("#userEdit input[name ='id']").val(data.permissionId);
+                    $("#userEdit").modal();
+                }else{
+                    layer.alert('系统默认最高权限不能移除！')
                 }
             }
         });

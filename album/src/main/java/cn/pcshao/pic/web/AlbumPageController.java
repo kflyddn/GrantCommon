25dfd622115ece6c1d8087a6c935fe5b83d7e436
pcshao <shaocover@163.com>
2018-12-23 00:23:40 +0800

    update album pageController.

--- album/src/main/java/cn/pcshao/pic/web/AlbumPageController.java
+++ album/src/main/java/cn/pcshao/pic/web/AlbumPageController.java
@@ -3,7 +3,7 @@ package cn.pcshao.pic.web;
 import cn.pcshao.grant.common.base.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 相册页面控制器
@@ -11,11 +11,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @date 2018-12-4
  */
 @Controller
+@RequestMapping("/album")
 public class AlbumPageController extends BaseController {
 
     @GetMapping("/albumManage")
     public String album(){
         return "albumManage";
+    }
+    @GetMapping("/albumSquare")
+    public String albumSquare(){
+        return "album";
     }
 
 }

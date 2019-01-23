19f17fd2f4d8e38a6047049cf8035b95f32eb38a
pcshao <shaocover@163.com>
2019-01-24 00:01:16 +0800

    for my 21 birthday. keep going.

--- gra_business/src/main/java/cn/pcshao/graduaction/BusinessApplication.java
+++ gra_business/src/main/java/cn/pcshao/graduaction/BusinessApplication.java
@@ -3,8 +3,7 @@ package cn.pcshao.graduaction;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.io.IOException;
@@ -13,7 +12,7 @@ import java.util.Properties;
 
 @SpringBootApplication(scanBasePackages = {"cn.pcshao.graduaction","cn.pcshao.grant.common", "cn.pcshao.pic"})
 @MapperScan("cn.pcshao.grant.common.dao")
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableScheduling
 //@EnableAsync //线程池异步，已经在commonApplication注解
 public class BusinessApplication {

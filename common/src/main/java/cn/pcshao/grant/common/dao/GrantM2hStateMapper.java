182a3267482a2e16efeadb82724afaf8978e436d
pcshao <shaocover@163.com>
2019-02-27 12:21:40 +0800

    add h2m table.

---
+++ common/src/main/java/cn/pcshao/grant/common/dao/GrantM2hStateMapper.java
@@ -0,0 +1,30 @@
+package cn.pcshao.grant.common.dao;
+
+import cn.pcshao.grant.common.entity.GrantM2hState;
+import cn.pcshao.grant.common.entity.GrantM2hStateExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface GrantM2hStateMapper {
+    int countByExample(GrantM2hStateExample example);
+
+    int deleteByExample(GrantM2hStateExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(GrantM2hState record);
+
+    int insertSelective(GrantM2hState record);
+
+    List<GrantM2hState> selectByExample(GrantM2hStateExample example);
+
+    GrantM2hState selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") GrantM2hState record, @Param("example") GrantM2hStateExample example);
+
+    int updateByExample(@Param("record") GrantM2hState record, @Param("example") GrantM2hStateExample example);
+
+    int updateByPrimaryKeySelective(GrantM2hState record);
+
+    int updateByPrimaryKey(GrantM2hState record);
+}
---
+++ common/src/main/java/cn/pcshao/grant/common/entity/GrantM2hState.java
@@ -0,0 +1,63 @@
+package cn.pcshao.grant.common.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class GrantM2hState implements Serializable {
+    private Long id;
+
+    private Long huserId;
+
+    private String state;
+
+    private Date createTime;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getHuserId() {
+        return huserId;
+    }
+
+    public void setHuserId(Long huserId) {
+        this.huserId = huserId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state == null ? null : state.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", huserId=").append(huserId);
+        sb.append(", state=").append(state);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}

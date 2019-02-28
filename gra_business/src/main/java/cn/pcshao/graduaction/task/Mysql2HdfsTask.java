e6f082c66b0bb3cbed27957db084669b1cf440ae
pcshao <shaocover@163.com>
2019-02-28 17:58:10 +0800

    update m2h task with local file generate and ready send it to hdfs.

--- gra_business/src/main/java/cn/pcshao/graduaction/task/Mysql2HdfsTask.java
+++ gra_business/src/main/java/cn/pcshao/graduaction/task/Mysql2HdfsTask.java
@@ -4,9 +4,10 @@ import cn.pcshao.grant.common.dao.GrantHuserMapper;
 import cn.pcshao.grant.common.dao.GrantM2hStateMapper;
 import cn.pcshao.grant.common.entity.GrantHuser;
 import cn.pcshao.grant.common.entity.GrantHuserExample;
-import cn.pcshao.grant.common.entity.GrantM2hStateExample;
-import cn.pcshao.grant.common.exception.CustomException;
 import cn.pcshao.grant.common.util.ListUtils;
+import cn.pcshao.grant.common.util.StringUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,7 +16,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 /**
@@ -33,6 +36,12 @@ public class Mysql2HdfsTask {
 
     @Value("${task.Mysql2Hdfs.switch}")
     private String taskSwitch;
+    @Value("${task.Mysql2Hdfs.tempFilePath}")
+    private String tempFilePath;
+    @Value("${task.Mysql2Hdfs.hadoopURI}")
+    private String hadoopURI;
+    @Value("${task.Mysql2Hdfs.hdfsLocate}")
+    private String hdfsLocatePath;
 
     @Resource
     private GrantHuserMapper huserMapper;
@@ -45,7 +54,6 @@ public class Mysql2HdfsTask {
             return;
         logger.debug("正在开始Mysql2Hdfs");
         GrantHuserExample huserExample = new GrantHuserExample();
-        GrantM2hStateExample stateExample = new GrantM2hStateExample();
     //读取huser存到临时文本
         //TODO 分批次读取 按ID读吧 bigint够用了
         Long maxHUserId = hStateMapper.getMaxHUserId();
@@ -58,15 +66,42 @@ public class Mysql2HdfsTask {
         if(ListUtils.isEmptyList(husers)) {
             return;
         }
-        List<String> dataList = obj2hdfs(husers);
+        File file = obj2file(husers, tempFilePath);
         //附加非结构化数据
         //文本与非结构化数据一同写入hdfs
-        
-        hStateMapper.insertBatch(husers);
+        write2hdfs(file, hdfsLocatePath);
+        //更新mysql
+//        hStateMapper.insertBatch(husers);
     }
 
-    private List<String> obj2hdfs(List<GrantHuser> husers) {
-        List<String> allData = new ArrayList<>();
+    private void write2hdfs(File file, String dstPath) {
+        URI uri = null;
+        try {
+            uri = new URI(hadoopURI);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        Configuration conf = new Configuration();
+        FileSystem fs = null;
+        String fileName = System.currentTimeMillis()+ StringUtils.getRandomString(5);
+        try {
+            fs = FileSystem.get(uri, conf);
+            //TODO 检测是否连接上
+            fs.copyFromLocalFile(new Path(file.getAbsolutePath()), new Path(dstPath+ fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File obj2file(List<GrantHuser> husers, String path) {
+        File file = new File(path);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        PrintWriter pw = new PrintWriter(fos);
         for (GrantHuser user: husers){
             StringBuilder sb = new StringBuilder();
             sb.append(user.getHuserId()+ " ");
@@ -75,8 +110,21 @@ public class Mysql2HdfsTask {
             sb.append(user.getSex()+ " ");
             sb.append(user.getTelephone()+ " ");
             sb.append(user.getEmail()+ " ");
-            allData.add(sb.toString());
+            pw.println(sb.toString());
         }
-        return allData;
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        pw.close();
+        if(fos != null) {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return file;
     }
 }

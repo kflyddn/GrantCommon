724894f2f8df46f1322fbb97bc1544d12c180d99
pcshao <shaocover@163.com>
2019-03-01 15:35:06 +0800

    operating excel.

--- gra_business/src/main/java/cn/pcshao/graduaction/service/impl/HUserServiceImpl.java
+++ gra_business/src/main/java/cn/pcshao/graduaction/service/impl/HUserServiceImpl.java
@@ -3,11 +3,13 @@ package cn.pcshao.graduaction.service.impl;
 import cn.pcshao.graduaction.service.HUserService;
 import cn.pcshao.grant.common.base.BaseDao;
 import cn.pcshao.grant.common.base.BaseServiceImpl;
+import cn.pcshao.grant.common.consts.DtoCodeConsts;
 import cn.pcshao.grant.common.dao.GrantHuserMapper;
 import cn.pcshao.grant.common.dao.GrantM2hStateMapper;
 import cn.pcshao.grant.common.entity.GrantHuser;
 import cn.pcshao.grant.common.entity.GrantHuserExample;
 import cn.pcshao.grant.common.entity.GrantM2hStateExample;
+import cn.pcshao.grant.common.exception.CustomException;
 import cn.pcshao.grant.common.util.PropertiesUtil;
 import org.springframework.stereotype.Service;
 
@@ -62,36 +64,40 @@ public class HUserServiceImpl extends BaseServiceImpl<GrantHuser, Long> implemen
         List<GrantHuser> users = new ArrayList<>();
         int colLength = list.get(0).size();
         int[] indexLocation = new int[colLength+1];
-        for (int i = 0; i < list.size(); i++) {
-            List row = list.get(i);
-            GrantHuser user = new GrantHuser();
-            for (int j = 0; j < row.size(); j++) {
-                if(indexLocation[0] != colLength){
-                    if(PropertiesUtil.getBusinessConfig("importHUsersTemplate.idCard").equals(row.get(j).toString())){
-                        indexLocation[1]=j;
-                        indexLocation[0]++;
-                    }else if(PropertiesUtil.getBusinessConfig("importHUsersTemplate.name").equals(row.get(j).toString())){
-                        indexLocation[2]=j;
-                        indexLocation[0]++;
-                    }else if(PropertiesUtil.getBusinessConfig("importHUsersTemplate.sex").equals(row.get(j).toString())){
-                        indexLocation[3]=j;
-                        indexLocation[0]++;
-                    }else if(PropertiesUtil.getBusinessConfig("importHUsersTemplate.email").equals(row.get(j).toString())){
-                        indexLocation[4]=j;
-                        indexLocation[0]++;
-                    }else if(PropertiesUtil.getBusinessConfig("importHUsersTemplate.telephone").equals(row.get(j).toString())){
-                        indexLocation[5]=j;
-                        indexLocation[0]++;
+        try {
+            for (int i = 0; i < list.size(); i++) {
+                List row = list.get(i);
+                GrantHuser user = new GrantHuser();
+                for (int j = 0; j < row.size(); j++) {
+                    if (indexLocation[0] != colLength) {
+                        if (PropertiesUtil.getBusinessConfig("importHUsersTemplate.idCard").equals(row.get(j).toString())) {
+                            indexLocation[1] = j;
+                            indexLocation[0]++;
+                        } else if (PropertiesUtil.getBusinessConfig("importHUsersTemplate.name").equals(row.get(j).toString())) {
+                            indexLocation[2] = j;
+                            indexLocation[0]++;
+                        } else if (PropertiesUtil.getBusinessConfig("importHUsersTemplate.sex").equals(row.get(j).toString())) {
+                            indexLocation[3] = j;
+                            indexLocation[0]++;
+                        } else if (PropertiesUtil.getBusinessConfig("importHUsersTemplate.email").equals(row.get(j).toString())) {
+                            indexLocation[4] = j;
+                            indexLocation[0]++;
+                        } else if (PropertiesUtil.getBusinessConfig("importHUsersTemplate.telephone").equals(row.get(j).toString())) {
+                            indexLocation[5] = j;
+                            indexLocation[0]++;
+                        }
+                        //如果增加字段往后加，自动记录下标标记
                     }
-                    //如果增加字段往后加，自动记录下标标记
                 }
+                user.setIdCard(row.get(indexLocation[1]).toString());
+                user.setName(row.get(indexLocation[2]).toString());
+                user.setSex(row.get(indexLocation[3]).toString().equals("1") ? true : false);
+                user.setEmail(row.get(indexLocation[4]).toString());
+                user.setTelephone(row.get(indexLocation[5]).toString());
+                users.add(user);
             }
-            user.setIdCard(row.get(indexLocation[1]).toString());
-            user.setName(row.get(indexLocation[2]).toString());
-            user.setSex(row.get(indexLocation[3]).toString().equals("1")?true:false);
-            user.setEmail(row.get(indexLocation[4]).toString());
-            user.setTelephone(row.get(indexLocation[5]).toString());
-            users.add(user);
+        }catch (Exception e){
+            throw new CustomException(DtoCodeConsts.EXCEL_FORMAT, DtoCodeConsts.EXCEL_FORMAT_MSG);
         }
         users.remove(0); //移除表头
         return users;

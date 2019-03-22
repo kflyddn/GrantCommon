cc37e61dcaa1b9b5600c1658f420150cc2015c25
pcshao <shaocover@163.com>
2019-03-22 08:54:58 +0800

    solve the bug,which scheduling opened could case the @EnableWebSocket annotation failed.

--- gra_business/src/main/java/cn/pcshao/graduaction/BusinessApplication.java
+++ gra_business/src/main/java/cn/pcshao/graduaction/BusinessApplication.java
@@ -4,17 +4,21 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-@SpringBootApplication(scanBasePackages = {"cn.pcshao.graduaction","cn.pcshao.grant.common", "cn.pcshao.pic"})
+@SpringBootApplication(scanBasePackages = {"cn.pcshao.graduaction","cn.pcshao.grant.common", "cn.pcshao.pic", "cn.pcshao.grant.websocket"})
 @MapperScan("cn.pcshao.grant.common.dao")
 //@EnableDiscoveryClient
 @EnableScheduling
 //@EnableAsync //线程池异步，已经在commonApplication注解
+//@EnableWebSocket  //开启WebSocket，已经在websocketApplication注解
 public class BusinessApplication {
 
     public static void main(String[] args) throws IOException {
@@ -25,6 +29,18 @@ public class BusinessApplication {
         SpringApplication app = new SpringApplication(BusinessApplication.class);
         app.setDefaultProperties(properties);
         app.run(args);
+    }
+
+    /**
+     * 定时任务注解会和websocket冲突
+     * 在注册一个合适的Scheduler失败时，直接创建了一个NoOpScheduler对象
+     * @Scheduled注解会调用这个没有任何意义的Scheduler,那么手动创建一个taskScheduler是不是就行了
+     * @return
+     */
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler(){
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        return taskScheduler;
     }
 
 }
